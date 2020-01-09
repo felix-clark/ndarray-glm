303e6335aaff6e0007d1602cecf4f3a64246eaa5
Felix Clark <mfclark3690@gmail.com>
2020-01-09 10:57:59 -0500

    format

--- src/linear.rs
+++ src/linear.rs
@@ -1,17 +1,22 @@
 //! Functions for solving linear regression
 
-use crate::utility::one_pad;
 use crate::error::RegressionError;
+use crate::utility::one_pad;
 use ndarray::{Array1, Array2};
 use ndarray_linalg::SolveH;
 
 /// data_y is an array of the y values, data_x is an array with rows indicating the data point and columns indicating the regressor
 /// Returns ordinary least squares solution of length 1 greater than the width of X
 /// TODO: return a result which includes validity and uncertainty information, rather than just the solution
-pub fn regression(data_y: &Array1<f32>, data_x: &Array2<f32>) -> Result<Array1<f32>, RegressionError> {
+pub fn regression(
+    data_y: &Array1<f32>,
+    data_x: &Array2<f32>,
+) -> Result<Array1<f32>, RegressionError> {
     let n_data = data_y.len();
     if n_data != data_x.nrows() {
-        return Err(RegressionError::BadInput("y and x must have same number of data points".to_string()));
+        return Err(RegressionError::BadInput(
+            "y and x must have same number of data points".to_string(),
+        ));
     }
     // prepend the x data with a constant 1 column
     let data_x = one_pad(data_x);
