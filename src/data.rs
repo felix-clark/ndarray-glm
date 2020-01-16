212b06cdb7da3606f99224ed6286b4a5d10f1956
Felix Clark <mfclark3690@gmail.com>
2020-01-16 13:38:51 -0500

    TODO comment

--- src/data.rs
+++ src/data.rs
@@ -123,6 +123,8 @@ where
         }
 
         // Check for co-linearity by ensuring that the determinant of X^T * X is non-zero.
+        // TODO: This check could be more precise if we subtract off the mean X
+        // first.
         let xtx: Array2<F> = self.data_x.t().dot(&self.data_x);
         let det: <<Array2<F> as DeterminantH>::Elem as Scalar>::Real = xtx.deth()?;
         let det: F = det.into();
