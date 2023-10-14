646d1266a90b12058f7baa83a7f2f056f3db82cb
Felix Clark <mfclark3690@gmail.com>
2023-10-14 15:41:02 -0400

    expanded test utility for multiple columns

--- tests/logistic.rs
+++ tests/logistic.rs
@@ -11,7 +11,7 @@ use common::{array_from_csv, y_x_off_from_csv};
 #[test]
 // this data caused an infinite loop with step halving
 fn log_termination_0() -> Result<()> {
-    let (y, x, off) = y_x_off_from_csv::<bool, f32>("tests/data/log_termination_0.csv")?;
+    let (y, x, off) = y_x_off_from_csv::<bool, f32, 1>("tests/data/log_termination_0.csv")?;
     let model = ModelBuilder::<Logistic>::data(&y, &x)
         .linear_offset(off)
         .build()?;
@@ -61,7 +61,7 @@ fn log_termination_0() -> Result<()> {
 #[test]
 // this data caused an infinite loop with step halving
 fn log_termination_1() -> Result<()> {
-    let (y, x, off) = y_x_off_from_csv::<bool, f32>("tests/data/log_termination_1.csv")?;
+    let (y, x, off) = y_x_off_from_csv::<bool, f32, 1>("tests/data/log_termination_1.csv")?;
     let model = ModelBuilder::<Logistic>::data(&y, &x)
         .linear_offset(off)
         .build()?;
@@ -73,7 +73,7 @@ fn log_termination_1() -> Result<()> {
 
 #[test]
 fn log_regularization() -> Result<()> {
-    let (y, x, off) = y_x_off_from_csv::<bool, f32>("tests/data/log_regularization.csv")?;
+    let (y, x, off) = y_x_off_from_csv::<bool, f32, 1>("tests/data/log_regularization.csv")?;
     // This can be terminated either by standardizing the data or by using
     // lambda = 2e-6 intead of 1e-6.
     // let x = ndarray_glm::standardize::standardize(x);
