62901e423643ac8a2a232bc133b08f0a0925fba3
Felix Clark <mfclark3690@gmail.com>
2023-10-18 18:07:06 -0400

    expanded test for weighted data

--- tests/R/logistic_weights.R
+++ tests/R/logistic_weights.R
@@ -2,6 +2,27 @@
 
 infile <- "../data/log_weights.csv"
 data <- read.csv(infile, header = FALSE)
+print(data)
 model <- glm(V1 ~ V2 + V3, data, family = "binomial", weights = V4)
+print(model)
 coefs <- model$coefficients
 write(coefs, file = "log_weights/coefficients.csv", ncolumns = 1)
+
+mod_sum <- summary(model)
+# For logistic the scaled and unscaled covariances are the same
+cov_mat <- mod_sum$cov.unscaled
+write(cov_mat, file = "log_weights/covariance.csv", ncolumns = 1)
+
+write(model$deviance, file = "log_weights/deviance.csv", ncolumns = 1)
+
+write(model$null.deviance, file = "log_weights/null_dev.csv", ncolumns = 1)
+
+write(
+  rstandard(model, type = "pearson"),
+  file = "log_weights/standard_pearson_resid.csv", ncolumns = 1
+)
+write(
+  rstandard(model, type = "deviance"),
+  file = "log_weights/standard_deviance_resid.csv", ncolumns = 1
+)
+write(rstudent(model), file = "log_weights/student_resid.csv", ncolumns = 1)
--- tests/weights.rs
+++ tests/weights.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 
 use approx::assert_abs_diff_eq;
+use ndarray::Array;
 use ndarray_glm::{Logistic, ModelBuilder};
 mod common;
 use common::{array_from_csv, y_x_off_from_csv};
@@ -8,16 +9,33 @@ use common::{array_from_csv, y_x_off_from_csv};
 #[test]
 fn logistic_weights() -> Result<()> {
     let (y, x, wts) = y_x_off_from_csv::<bool, f32, 2>("tests/data/log_weights.csv")?;
-    dbg!(&y);
-    dbg!(&x);
     let model = ModelBuilder::<Logistic>::data(&y, &x).weight(wts).build()?;
     let fit = model.fit()?;
 
     let r_result = array_from_csv::<f32>("tests/R/log_weights/coefficients.csv")?;
-    // TODO: try different epsilons
-    assert_abs_diff_eq!(&fit.result, &r_result, epsilon=1e-5);
-    assert!(fit.lr_test_against(&r_result) >= 0., "make sure our fit is at least as good as R's");
+    // NOTE: R result only seems good to a few decimal points
+    assert_abs_diff_eq!(&fit.result, &r_result, epsilon = 1e-4);
+    assert!(
+        fit.lr_test_against(&r_result) >= 0.,
+        "make sure our fit is at least as good as R's"
+    );
 
-    // TODO: deviance, covariance?
+    let n_par = fit.result.len();
+
+    // check parameter covariance function
+    let r_flat_cov = array_from_csv::<f32>("tests/R/log_weights/covariance.csv")?;
+    let r_cov = Array::from_shape_vec((n_par, n_par), r_flat_cov.into_raw_vec())?;
+    assert_abs_diff_eq!(*fit.covariance()?, r_cov, epsilon = 1e-4);
+
+    // total deviance uses the weights
+    let r_dev = array_from_csv::<f32>("tests/R/log_weights/deviance.csv")?[0];
+    assert_abs_diff_eq!(fit.deviance(), r_dev, epsilon = 1e-5);
+
+    let r_null_dev = array_from_csv::<f32>("tests/R/log_weights/null_dev.csv")?[0];
+    // assert_abs_diff_eq!(fit.null_like(), r_null_dev, epsilon = eps); // these are probably not
+    // the same
+    // likelihood ratio test should be positive
+    assert_abs_diff_eq!(fit.lr_test(), r_null_dev - r_dev, epsilon = 1e-5);
+
     Ok(())
 }
