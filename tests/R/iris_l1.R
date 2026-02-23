25baf6caf51ff224431352c158f744d82f2f468e
Felix Clark <mfclark3690@gmail.com>
2026-02-23 08:35:31 -0500

    updating R tests and results

--- tests/R/iris_l1.R
+++ tests/R/iris_l1.R
@@ -10,22 +10,20 @@ x_data <- data[
   ,
   c("sepal_length", "sepal_width", "petal_length", "petal_width")
 ]
-# scale externally so that we can match the operation
-x_data <- scale(x_data)
 # This class is seperable, so this will test regularization
 # y_data <- data["class"] == "setosa"
 y_data <- data["class"] == "versicolor"
 l1 <- 1e-2 / length(y_data)
+alpha = 1
+
+# Do the model with standardization
 model <- glmnet(
   x_data, y_data,
-  # Standardization is recommended particularly for L1, although the result is
-  # re-scaled anyway.
-  standardize = FALSE,
+  standardize = TRUE,
   # the LASSO penalty
-  alpha = 1,
+  alpha = alpha,
   # With this dataset, a large lambda zeros out all coefficients
   lambda = l1,
-  # lambda = 0,
   thresh = 1e-20,
   family = "binomial",
 )
@@ -33,3 +31,19 @@ beta <- coef(model)
 print(beta)
 beta <- beta[, "s0"]
 write(beta, file = "log_regularization/iris_versicolor_l1_1e-2.csv", sep = "\n")
+
+# Then do the model without standardization
+model <- glmnet(
+  x_data, y_data,
+  standardize = FALSE,
+  # the LASSO penalty
+  alpha = alpha,
+  # With this dataset, a large lambda zeros out all coefficients
+  lambda = l1,
+  thresh = 1e-20,
+  family = "binomial",
+)
+beta <- coef(model)
+print(beta)
+beta <- beta[, "s0"]
+write(beta, file = "log_regularization/iris_versicolor_l1_1e-2_nostd.csv", sep = "\n")
